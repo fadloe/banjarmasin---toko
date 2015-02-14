734ecee987db706b490830403bf60021e51773a3
L0sTBoY <robby.keren@gmail.com>
2015-02-15 04:36:50 +0800

    updated halam pertama
    updated halam pertama

---
+++ ukm/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> <head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<title>Fadloe Robbi</title> 
+<style type="text/css">     
+#halaman { width:1080px; margin:0 auto; }     
+#header { height:100px; padding:10px;background-color:#CCCCCC; }
+#header-bawah { height:10px; padding:10px;background-color:#ACCCCC; }     
+#kiri { height:550px; padding:10px;background-color:#CCCCCC;float:left;width:200px; margin-top:10px; }     
+    
+#footer { clear:both; height:350px;padding:10px; background-color:#CBBCCC; ;  ; margin-top:10px;} </style> 
+</head> 
+	<body> 
+		<div id="halaman"> 
+			<div id="header">HEADER</div>
+			<div id="header-bawah">HEADER BAWAH</div>
+			<div id="kiri">KIRI</div>
+			<div id="footer">footer</div>
+	</body> 
